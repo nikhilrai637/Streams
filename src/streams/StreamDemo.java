79825b075e6a888f56451f65182d2f437f5e22c8
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-27 00:56:24 +0530

    creating streams from arrays

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -1,26 +1,11 @@
 package streams;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void show(){
-         List<Movie>movieList = List.of(
-                    new Movie("a",10),
-                    new Movie("b",11),
-                    new Movie("d",5)
-         );
-
-         int count = 0;
-         //Imperative Programming
-         for(var i = 0 ; i<movieList.size();i++){
-              if(movieList.get(i).getLikes() > 10 )
-                  count++;
-         }
-        System.out.println(count);
-
-         //Declarative Functional Programming
-         var count2  = movieList.stream().filter(mv -> mv.getLikes() > 10).count();
-        System.out.println(count2);
+          int[] numbers = {1,2,3};
+        Arrays.stream(numbers).forEach(n -> System.out.println(n));
     }
-
 }
