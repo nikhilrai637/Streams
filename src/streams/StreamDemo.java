4e410d4aef4a9f0fa28b0f8f7889857f5dcf25e6
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-27 01:06:26 +0530

    streams from arbitary collection of objects

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -1,11 +1,17 @@
 package streams;
+import java.io.Serializable;
+import java.lang.constant.Constable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void show(){
-          int[] numbers = {1,2,3};
-        Arrays.stream(numbers).forEach(n -> System.out.println(n));
+         //Streams from arbitary number of parameters
+        var count =   Stream.of(1, "2324", 2)
+                        .filter(v ->  ((Constable) v)
+                                .getClass().isPrimitive() )
+                        .count();
+        System.out.println(count);
     }
 }
