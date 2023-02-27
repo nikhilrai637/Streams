501928111d081085cfad6d7b6be154a9988ed61b
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-27 22:10:01 +0530

    filtering Elements

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -3,13 +3,22 @@ import java.io.Serializable;
 import java.lang.constant.Constable;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void show(){
-       var stream = Stream.of(List.of(1,2,3),List.of(4,5,6));
-       stream  .flatMap(list-> list.stream())
-               .forEach(n-> System.out.println(n));
+       List<Movie>movieList = List.of(new Movie("a",9)
+                            ,new Movie("b",10)
+                            ,new Movie("c",12)
+                            ,new Movie("d",29));
+
+        Predicate<Movie> isPopular = movie -> movie.getLikes()>10;
+       movieList.stream()
+                .filter(isPopular)
+                .forEach(movie -> System.out.println(movie.getTitle()));
+
+
 
     }
 }
