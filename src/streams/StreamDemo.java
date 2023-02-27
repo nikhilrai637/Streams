5b98ec77057d0acba41dc1cc37d1e33c10473fa0
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-27 22:52:19 +0530

    sorting on the basis of Comparator

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -2,28 +2,22 @@ package streams;
 import java.io.Serializable;
 import java.lang.constant.Constable;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void show(){
-       List<Movie>movieList = List.of(new Movie("a",9)
+       List<Movie>movieList = List.of(new Movie("z",9)
                             ,new Movie("b",10)
-                            ,new Movie("c",30)
+                            ,new Movie("y",30)
                             ,new Movie("d",20));
-        //1000 movies
-        //10 movies per page
-        //3rd page
-        // skip - skip(pagenum-1)*pagesize
-        // limit pagesize
 
-        movieList.stream()
-                .takeWhile(m -> m.getLikes()<30)
-                .forEach(m -> System.out.println(m.getTitle()));
+       movieList.stream()
+               .sorted((a,b)->a.getTitle().compareTo(b.getTitle()))
+               .sorted(Comparator.comparing(Movie::getTitle).reversed())
+               .forEach(m-> System.out.println(m.getTitle()));
 
-        movieList.stream()
-                .dropWhile(m -> m.getLikes()<30)
-                .forEach(m -> System.out.println(m.getTitle()));
     }
 }
