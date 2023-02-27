91aa2e51faf26036fabb189d002c8579a535c412
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-27 23:00:09 +0530

    getting unique elements

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -11,13 +11,14 @@ public class StreamDemo {
     public static void show(){
        List<Movie>movieList = List.of(new Movie("z",9)
                             ,new Movie("b",10)
+                            ,new Movie("b",10)
                             ,new Movie("y",30)
                             ,new Movie("d",20));
 
        movieList.stream()
-               .sorted((a,b)->a.getTitle().compareTo(b.getTitle()))
-               .sorted(Comparator.comparing(Movie::getTitle).reversed())
-               .forEach(m-> System.out.println(m.getTitle()));
+                .map(Movie::getLikes)
+                .distinct()
+                .forEach(System.out::println);
 
     }
 }
