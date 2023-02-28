c56e83a6a8064450f3198002c0311bebf7772ec6
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-28 17:29:31 +0530

    using Collectors

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -6,19 +6,20 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void show() {
         List<Movie> movieList = List.of(new Movie("z", 9)
                 , new Movie("b", 10)
-                , new Movie("b", 10)
+                , new Movie("t", 10)
                 , new Movie("y", 30)
                 , new Movie("d", 20));
 
         var result = movieList.stream()
-                .map(Movie::getLikes)
-                .reduce(0, Integer::sum);
+
+                        .collect(Collectors.toMap(Movie::getTitle,Movie::getLikes));
 
 
         System.out.println(result);
