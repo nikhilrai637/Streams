c180292e1236f27b7b5335ddd71cb2e5e810d25b
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-28 21:45:10 +0530

    Primitive Streams use cases

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -3,20 +3,15 @@ package streams;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamDemo {
     public static void show() {
-        List<Movie> movieList = List.of(new Movie("z", 9, Genre.ACTION)
-                , new Movie("b", 10, Genre.COMEDY)
-                , new Movie("t", 10, Genre.COMEDY)
-                , new Movie("y", 30, Genre.THRILLER)
-                , new Movie("d", 20, Genre.ACTION));
+        IntStream.range(1,5).
+                forEach(System.out::println);
 
-       var result =  movieList.stream()
-                        .collect(Collectors.partitioningBy(movie -> movie.getLikes()>10,
-                                Collectors.mapping(Movie::getTitle,
-                                        Collectors.joining(","))));
+        IntStream.rangeClosed(1,5).
+                forEach(System.out::println);
 
-        System.out.println(result);
     }
 }
